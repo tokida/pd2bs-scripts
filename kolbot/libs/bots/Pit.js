1b211924903d9182e4e71d9350e191bff0777157
hoge <hoge@local.com>
2021-04-14 21:33:06 +0900

    fix party(MFLeader)

--- kolbot/libs/bots/Pit.js
+++ kolbot/libs/bots/Pit.js
@@ -5,12 +5,40 @@
 */
 
 function Pit() {
+
+
+	if (Config.MFLeader) {
+		me.overhead("waiting for party");
+		say("waiting for party");
+		delay(2000);
+		say("3");
+	}
+
+	print("doChores");
 	Town.doChores();
+
+	print("getCorpse");
+	Town.getCorpse();
+
+	//Town.clearInventory();
 	Pather.useWaypoint(6);
+
+
+	print("doprecast");
 	Precast.doPrecast(true);
+
+	//Pather.makePortal();
+	//delay(10000);
+
+	print("My area is " + me.area)
+	print("ClearType :" + Config.ClearType)
+	Config.ClearType = 0x0;
 
 	if (!Pather.moveToExit([7, 12], true)) {
 		throw new Error("Failed to move to Pit level 1");
+	}
+	if (Config.MFLeader) {
+		delay(10000);
 	}
 
 	if (Config.Pit.ClearPit1) {
@@ -20,8 +48,9 @@ function Pit() {
 	if (!Pather.moveToExit(16, true, Config.Pit.ClearPath)) {
 		throw new Error("Failed to move to Pit level 2");
 	}
+	delay(10000);
 
-	Attack.clearLevel();
+	Attack.clearLevel(0x0);
 
 	return true;
 }
